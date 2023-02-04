38e031ae781e15aa36800ea0f966be40e38f85be
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2023-02-05 03:33:53 +0600

    Add files via upload

---
+++ 001 Night1/001 Night1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System; 
+
+namespace _001_Night1
+{
+    class night
+    {
+        double time;
+        string foodName;
+
+        night(double time, string foodName)
+        {
+            this.time = time;
+            this.foodName = foodName;
+        }
+
+        void setTiime(double time)
+        {
+            this.time = time;
+        }
+        double getTiime()
+        {
+            return this.time;
+        }
+
+        void setFoodname(string foodName)
+        {
+            this.foodName = foodName;
+        }
+
+        string getFoodName()
+        {
+            return this.foodName;
+        }
+
+        void display()
+        {
+            
+            Console.WriteLine("Time : " + time+" AM ");
+            Console.WriteLine("Food : " + foodName);
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am not going to print that Cz I hate programming ");
+            night n1 = new night (2.11,"Burger");
+            n1.display();
+        }
+    }
+
+}
