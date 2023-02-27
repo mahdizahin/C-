4a8c87429e2b1c4cc36e5e7539e388ac50749d2b
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2023-02-27 16:45:42 +0600

    Add files via upload

---
+++ Array/Array/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             int[] arr = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Enter Number at index ["+i+"] : ");
+                string input = Console.ReadLine();
+                arr[i] = int.Parse(input);
+            }
+            Console.WriteLine("\n\nOutput is : " );
+            for(int i = 0; i < 10; i++)
+            {
+                Console.Write(arr[i]+" ");
+            }
+
+        }
+    }
+}
