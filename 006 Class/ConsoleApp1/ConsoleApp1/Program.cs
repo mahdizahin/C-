b3e9b5c5fb27b80e1ae27007f3d6aaee1f496c87
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2023-02-27 17:05:01 +0600

    Add files via upload

---
+++ 006 Class/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+        class dhong
+    {
+        int dhongID;
+        string dhongName;
+
+        
+
+        public dhong(int dhongID, string dhongName)
+        {
+            this.dhongID = dhongID; 
+            this.dhongName = dhongName;
+        }
+
+       public void display()
+        {
+            Console.WriteLine("Dhong ID : "+dhongID);
+            Console.WriteLine("Dhong Name : "+dhongName);
+        }
+    }
+      class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Dhong ID : ");
+            int dhongId = int.Parse(Console.ReadLine());
+            Console.Write("Enter Dhong Name : ");
+            string dhongName = Console.ReadLine();
+
+            dhong nekamo = new dhong(dhongId, dhongName);
+            nekamo.display();
+        }
+    }
+}
