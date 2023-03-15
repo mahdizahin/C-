1e94dccb4c5808cdd4a5120b57cdb52147acb755
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2023-03-15 15:04:29 +0600

    Add files via upload

---
+++ Polymorphism/Polymorphism/GoruGari.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class GoruGari : Gari
+    {
+        private string region = "Indian";
+
+        public string Model { get; set; }
+        public GoruGari(int power, string color, string Model) : base(power, color)
+        {
+            this.Model = Model;
+        }
+
+        public void display()
+        {
+            base.display();
+            Console.WriteLine(region);
+        }
+
+        public void thikKora()
+        {
+            Console.WriteLine(Model + " gari thik kora done");
+        }
+
+    }
+
+
+
+    
+}
---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+ 
+
+namespace Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey, Zahin!\n Whats the newx");
+
+            Gari c1 = new Gari(7,"pink");
+            c1.display();
+            c1.thikKora();
+
+            Console.WriteLine("\n\n");
+
+            GoruGari lalmia = new GoruGari(2,"Black","Mandhat Amoler");
+            lalmia.display();
+            lalmia.thikKora();
+
+            Console.WriteLine("\n\n");
+
+            Gari Mukkurgari = new GhuraGari(1, "Halka nal wong", "Teji Ghura");
+            Mukkurgari.display();
+            Mukkurgari.thikKora();
+
+        }
+    }
+}
