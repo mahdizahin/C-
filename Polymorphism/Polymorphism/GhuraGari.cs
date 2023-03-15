49023324e6a91d2a4c039fb5e7de79dda6b33e51
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2023-03-15 15:12:02 +0600

    Add files via upload

---
+++ Polymorphism/Polymorphism/GhuraGari.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+     public class GhuraGari : Gari
+    {
+
+        private string jat="Turkish";
+
+        public string model { get; set; }
+
+        /*public GhuraGari(int power, string color;  string model ):base(power,color)
+        {
+            this.model = model;
+        }
+        */
+
+        public string Model { get; set; }
+        public GhuraGari(int power, string color, string Model) : base(power, color)
+        {
+            this.Model = Model;
+        }
+
+        public void display()
+        {
+            Console.WriteLine(jat);
+        }
+
+        public void thikKora()
+        {
+            Console.WriteLine(Model + " thik kora hoise");
+        }
+
+
+
+
+    }
+}
