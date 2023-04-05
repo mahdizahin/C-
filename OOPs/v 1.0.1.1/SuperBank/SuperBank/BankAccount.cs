6082000a4d875df5043bb9baaffdebdb19e38fd8
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2023-04-06 00:25:05 +0600

    Add files via upload

---
+++ OOPs/v 1.0.1.1/SuperBank/SuperBank/BankAccount.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperBank
+{
+     public class BankAccount
+    {
+        public string number { get; set; }
+        public string owner { get; set; }
+        public decimal balance { get; set; }
+
+        public BankAccount(string owner, decimal balance) {
+            this.owner = owner;
+            this.balance = balance;
+        }
+        public void deposit(string number, string owner, decimal balance)
+        {
+            this.number = number;
+            this.owner = owner; 
+            this.balance= balance;
+            
+        }
+
+        public void withdraw(string number, string owner, decimal balance)
+        {
+            this.number = number;
+            this.owner = owner;
+            this.balance = balance;
+
+        }
+
+
+        public void show()
+        {
+            Console.WriteLine(owner+ " " + balance);
+        }
+
+
+
+    }
+}
---
+++ OOPs/v 1.0.1.1/SuperBank/SuperBank/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var account = new BankAccount("dotnet", 5000);
+
+            account.show();
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
