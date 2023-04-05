8231f401186e6f41a7010c088114f609d3bdc659
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2023-04-06 00:34:12 +0600

    Add files via upload

---
+++ OOPs/BloodBankBeta  v.1.0.1/BloodBankBeta/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodBankBeta
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public Employee(int id, string name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public void display()
+        {
+            Console.WriteLine("EMP ID : "+id+"\nEMP NAME : "+name);
+        }
+    }
+}
---
+++ OOPs/BloodBankBeta  v.1.0.1/BloodBankBeta/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodBankBeta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee sinC = new Employee(991234,"Clorin HeinNan");
+            sinC.display();
+        }
+    }
+}
