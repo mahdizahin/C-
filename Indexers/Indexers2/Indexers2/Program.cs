46eecc5ad10a9ce49a1d9c76f7d691392bc8a9a9
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2023-04-18 21:02:50 +0600

    Add files via upload

---
+++ Indexers/Indexers2/Indexers2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers2
+{
+    public class orus
+    {
+        public int[] roll = new int[4];
+
+        public int this[int i]
+        {
+            get { return roll[i]; }
+            set { roll[i] = value; }
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            orus obj1 = new orus();
+            obj1[0] = 78;
+            obj1[1] = 70;
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.Write("Value At index [" + i +"] : ");
+                Console.WriteLine(obj1[i]);
+            }
+        }
+    }
+}
